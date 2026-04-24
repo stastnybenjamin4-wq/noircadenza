e78501730cc5431aa2b6159048a64d51f815044c
Benjamin Šťastný <stastnybenjamin4@gmail.com>
2026-04-24 13:39:08 +0200

    Added ease in and out on the em of h1

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ let typewriterListTracker = 0;
 
 //Increase the typewriterListTracker by 1
 function nextElement() {
-    typewriterElement.textContent = typewriterList[typewriterListTracker];
-    typewriterListTracker += 1;
-    if (typewriterListTracker >= typewriterList.length) {
-        typewriterListTracker = 0;
-    }
+    typewriterElement.style.opacity = 0;    
+        setTimeout(function() {
+            
+            typewriterListTracker += 1;
+            if (typewriterListTracker >= typewriterList.length) {
+                typewriterListTracker = 0;
+            }
+            typewriterElement.textContent = typewriterList[typewriterListTracker];
+            typewriterElement.style.opacity = 1;
+        }, 700);
 }
-setInterval(nextElement, 2000);
+setInterval(nextElement, 3000);
